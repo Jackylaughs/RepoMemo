f2873db16369e00f7d1710d7fb56d9a7e9e64c73
Jackylaughs <hirarin412@gmail.com>
2026-05-31 05:33:22 +0900

    ミュート機能を実装

--- global.js
+++ global.js
@@ -1,19 +1,67 @@
-// audio
+// Audio
 
 const click_sound = document.getElementById ("repo_click");
 const hover_sound = document.getElementById ("repo_hover");
 
 const anchor = document.querySelectorAll ("a");
+const mute_button = document.querySelector (".mute_switch");
 
-anchor.forEach (function (link) {
+const audios = document.querySelectorAll ("audio");
+
+document.addEventListener ("DOMContentLoaded", function () { // ページを読み込んだ時
+    const storageMute = localStorage.getItem ("isMuted");
+
+    if (storageMute === "true") { // もしローカルストレージのmuteが真ならミュートにする
+        audios.forEach (function (audio) {
+            audio.volume = 0;
+        });
+    } else {
+        audios.forEach ( function (audio) {
+            audio.volume = 0.75;
+            mute_button.classList.remove ("muted");
+        });
+    }
+
+    click_sound.currentTime = 0;
+    click_sound.play();
+});
+
+mute_button.addEventListener ("click", function() { // ミュートボタンをクリックした時
+    mute_button.classList.toggle ("muted");
+    const isMuted = mute_button.classList.contains ("muted");
+
+    if (isMuted === true) {
+        audios.forEach (function (audio) {
+            audio.volume = 0;
+        });
+    } else {
+        audios.forEach (function (audio) {
+            audio.volume = 0.75;
+        });
+    }
+
+    localStorage.setItem ("isMuted", isMuted)
+
+    click_sound.currentTime = 0;
+    click_sound.play();
+
+    console.log (isMuted);
+    console.log (localStorage.getItem ("isMuted"));
+});
+
+anchor.forEach (function (link) { // リンクにホバーした時
     link.addEventListener ("mouseenter", function() {
         hover_sound.currentTime = 0;
-        hover_sound.volume = 0.75;
         hover_sound.play();
     });
 });
 
-document.addEventListener ("DOMContentLoaded", function() {
-    click_sound.volume = 0.75;
-    click_sound.play();
-})
+mute_button.addEventListener ("mouseenter", function() { // ミュートボタンにホバーした時
+        hover_sound.currentTime = 0;
+        hover_sound.play();
+});
+
+
+// 敵の体力　武器の威力　貴重品の重さ moonとかレベル
+
+// 敵　マップ　貴重品　スキン
